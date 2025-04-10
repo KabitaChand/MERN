7e90806dec3da8277b0330b0a69e718857b14b4c
kabita chand <ckabita21@gmail.com>
2025-04-10 22:03:04 +0545

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -102,5 +102,26 @@ export const loginUser = async (req, res) => {
   }
 };
 
-
-  
+// get all users 
+export const getAllUsers=async(req,res)=>{
+  try{
+    const users =await User.find();
+    res.json(users);
+  }
+  catch(error){
+    res.status(500).json({message:"No Users Found"});
+  }
+};
+export const logoutUser = (req, res) => {
+  try {
+      // Clear the token cookie
+      res.clearCookie('token', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: 'strict'
+      });
+      res.status(200).json({ message: "Logout successful!" });
+  } catch (err) {
+      res.status(500).json({ message: "Something went wrong!" });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,13 @@
 //userRoutes.js
-import {registerUser,loginUser} from "../controllers/userController.js";
+import {registerUser,loginUser, getAllUsers, logoutUser} from "../controllers/userController.js";
 import express from "express";
+import User from "../models/user.js";
 
 const router= express.Router();
 router.post('/signup',registerUser);
 router.post('/login',loginUser);
+router.get('/getUsers',getAllUsers);
+router.get('/logout', logoutUser);
 
 
 export default router;
